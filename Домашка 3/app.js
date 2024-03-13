728def0296b38d0d6168c49f05234bf2029a9313
anlzv <nastysha.laz@gmail.com>
2024-03-13 22:03:16 +0800

    Домашка 3

---
+++ Домашка 3/app.js
@@ -0,0 +1,14 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "Шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 180;
+let percentage = 14;
+let fullPrice = 367;
+let responsive = true;
+
+
+console.log(titleProject)
+console.log(screensValue)
+console.log(screenPrice)
+console.log(percentage)
+console.log(fullPrice)
+console.log(responsive)

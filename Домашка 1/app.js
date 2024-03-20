05f0ca976eb887fea2ff1d1cd61350b1b150cc8b
anlzv <nastysha.laz@gmail.com>
2024-03-20 18:46:17 +0800

    Исправила названия папок

---
+++ Домашка 1/app.js
@@ -0,0 +1,5 @@
+let firstName = "Anastasiia";
+let age = 25;
+let city = "Ubud";
+let hobby = "scrapbooking, travel"
+console.log(firstName, age, city, hobby)
--- Домашка 4/app.js
+++ Домашка 4/app.js
@@ -17,17 +17,48 @@ let servicePrice1 = prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой еще нужен сервис?");
 let servicePrice2 = prompt("Сколько будет стоить второй сервис?");
 
-fullPrice = screenPrice + +servicePrice1 + +servicePrice2;
-servicePercentPrice = Math.round(fullPrice - (percentage * fullPrice) / 100);
+// fullPrice = screenPrice + +servicePrice1 + +servicePrice2;
+// servicePercentPrice = Math.round(fullPrice - (percentage * fullPrice) / 100);
 
-if (fullPrice >= 50000) {
-  console.log("Скидка 10%");
-} else if (fullPrice < 50000 && fullPrice >= 20000) {
-  console.log("Скидка 5%");
-} else if (fullPrice < 20000 && fullPrice >= 0) {
-  console.log("Скидка не предусмотрена");
-} else if (fullPrice < 0) {
-  console.log("Что-то пошло не так");
+// if (fullPrice >= 50000) {
+//   console.log("Скидка 10%");
+// } else if (fullPrice < 50000 && fullPrice >= 20000) {
+//   console.log("Скидка 5%");
+// } else if (fullPrice < 20000 && fullPrice >= 0) {
+//   console.log("Скидка не предусмотрена");
+// } else if (fullPrice < 0) {
+//   console.log("Что-то пошло не так");
+// }
+
+let getAllServicePrices = function () {
+  return +servicePrice1 + +servicePrice2;
+};
+let allServicePrices = getAllServicePrices();
+
+function getFullPrice() {
+  return +allServicePrices + +screenPrice;
+}
+fullPrice = getFullPrice();
+
+function getTitle() {
+  return (
+    titleProject[0].toUpperCase() + titleProject.substring(1).toLowerCase()
+  );
+}
+titleProject = getTitle();
+
+function getServicePercetPrice() {
+  return Math.round(fullPrice - (percentage * fullPrice) / 100);
+}
+servicePercentPrice = getServicePercetPrice();
+
+function getRrollbackMessage() {
+  if (fullPrice >= 50000) return "Скидка 10%";
+  else if (fullPrice < 50000 && fullPrice >= 20000) {
+    return "Скидка 5%";
+  } else if (fullPrice < 20000 && fullPrice >= 0) {
+    return "Скидка не предусмотрена";
+  } else if (fullPrice < 0) return "Что-то пошло не так";
 }
 
 console.log("titleProject: " + titleProject);
@@ -41,3 +72,4 @@ console.log("servicePrice1: " + servicePrice1);
 console.log("service2: " + service2);
 console.log("servicePrice2: " + servicePrice2);
 console.log("servicePercentPrice: " + servicePercentPrice);
+console.log(getRrollbackMessage());
